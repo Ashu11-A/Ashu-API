18126e8021849551a1ca8f7ac1f16c0cf35d1d7a
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-08-28 16:12:24 -0400

    esse teste não deveria estar aqui

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,14 +3,13 @@ import { App } from "./app"
 import request from 'supertest';
 
 describe('Test API', () => {
-    it('BackupInfo', async () => {
+    test('BackupInfo', async () => {
         const res = await request(new App().server).get('/backup-size')
         expect(res.status).toEqual(200)
         expect(res.statusCode).toEqual(200)
-        expect(res.body).toHaveProperty('backup')
     })
 
-    it('SystemInfo', async () => {
+    test('SystemInfo', async () => {
         const res = await request(new App().server).get("/system-info")
         expect(res.status).toEqual(200)
         expect(res.statusCode).toEqual(200)
